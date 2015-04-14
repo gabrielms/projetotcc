809e02b819a7baea62a36dd5d77b12a73bd8f92f
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2015-04-13 21:01:07 -0300

    Ajustado validação de quem pode responder questionário

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,7 @@
 class RepliesController < ApplicationController
   before_action :set_poll
   before_action :set_coach
+  before_action :ensure_is_coach
 
   def new
     @reply = @poll.replies.build
@@ -23,12 +24,16 @@ class RepliesController < ApplicationController
 
   private
 
+  def ensure_is_coach
+    redirect_to polls_path, alert: "You have to be a Coach to take the Poll" unless current_user.kind == "coach"
+  end
+
   def set_poll
     @poll = Poll.find params[:poll_id]
   end
 
   def set_coach
-    @coach = Coach.find(current_user.kind == "coach" ? current_user.coach.id : 1)
+    @coach = Coach.find current_user.coach.id if current_user.kind == "coach"
   end
 
   def reply_params
