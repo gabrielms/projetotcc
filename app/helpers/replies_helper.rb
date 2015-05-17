b84fd984a97f8d97a696a3f0451274dfef4802c7
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2015-05-17 19:12:16 -0300

    Ajustadas perguntas para ficarem descritivas

--- app/helpers/replies_helper.rb
+++ app/helpers/replies_helper.rb
@@ -1,2 +1,13 @@
 module RepliesHelper
+	def get_value_for_answer_title(title_id)
+		case title_id
+		when "1" then "Não deve ser feito"
+		when "2" then "Levemente recomendado"
+		when "3" then "Recomendado"
+		when "4" then "Muito recomendado"
+		when "5" then "Obrigatório"
+		else
+			"Não encontrado"
+		end
+	end
 end
